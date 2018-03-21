a75c6ca7d0ae3b9745056a2466e7ed6f5a363c65
vietlubu <baoviet3010@gmail.com>
2018-03-21 14:50:02 +0700

    idnum2itemdesctable

--- idindesc/js.js
+++ idindesc/js.js
@@ -10,14 +10,57 @@ function addId(type) {
     button.innerHTML = "Download";
     document.getElementById("download").appendChild(button);
 
+    document.getElementById('loadding').classList.remove('hidden');
+
     if (type == "iteminfo") {
-        document.getElementById('loadding').classList.remove('hidden');
         iteminfo(file);
+    } else if (type == "idnum2itemdesctable") {
+        idnum2itemdesctable(file);
+    }
+}
+
+function idnum2itemdesctable(file) {
+    var reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = function (evt) {
+        let items = evt.target.result.split('\n#');
+        let regexId = /(\d{1,})[\#]/;
+        let itemId = 0;
+        let idnum2itemdesctable = '';
+
+        for (let iItem = 0; iItem < items.length; iItem++) {
+            let itemLines = items[iItem].split('\n');
+
+            for (let iLine = 0; iLine < itemLines.length; iLine++) {
+                matchId = itemLines[iLine].match(regexId);
+                if (matchId) {
+                    itemId = matchId["1"];
+                    itemLines.push('ID :^777777 ' + itemId + '^000000');
+                }
+            }
+            idnum2itemdesctable = idnum2itemdesctable + itemLines.join('\n');
+
+            if (iItem < items.length-1) {
+                idnum2itemdesctable = idnum2itemdesctable + '\n#';
+            }
+        }
+
+        // idnum2itemdesctable
+        document.querySelector('.output .filename').innerHTML = 'idnum2itemdesctable.txt';
+
+        document.getElementById('loadding').classList.add('hidden');
+        document.getElementById('output').classList.remove('hidden');
+
+        document.querySelector("#download button").addEventListener("click", function(event){
+            let blob = new Blob([idnum2itemdesctable], {type: "text/plain"});
+            saveAs(blob, "idnum2itemdesctable.txt");
+        }, false);
+
     }
 }
 
 function iteminfo(file) {
-    
+
     var reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = function (evt) {
@@ -39,18 +82,18 @@ function iteminfo(file) {
                 indexDesc = index+1;
                 let itemDesc = [];
                 itemDesc.push(itemInfoData[index]);
-                for (indexDesc; indexDesc < itemInfoData.length; indexDesc++) {                
+                for (indexDesc; indexDesc < itemInfoData.length; indexDesc++) {
                     if (itemInfoData[indexDesc].match(regexEndBlock)) {
                         itemDesc[itemDesc.length-1] = itemDesc[itemDesc.length-1] + ',';
                         if (itemID) {
-                            itemDesc.push(itemDesc[itemDesc.length-1].replace(itemDesc[itemDesc.length-1].replace(/\s+/, ""), '"ID : ^777777' + itemID + '^000000"'));
+                            itemDesc.push(itemDesc[itemDesc.length-1].replace(itemDesc[itemDesc.length-1].replace(/\s+/, ""), '"ID :^777777 ' + itemID + '^000000"'));
                         }
-                        itemDesc.push(itemInfoData[indexDesc]);                        
+                        itemDesc.push(itemInfoData[indexDesc]);
                         break;
                     }
 
                     itemDesc.push(itemInfoData[indexDesc]);
-                }                
+                }
                 iteminfo = iteminfo + itemDesc.join('\n') + '\n';
 
                 index = indexDesc;
@@ -71,6 +114,6 @@ function iteminfo(file) {
             let blob = new Blob([iteminfo], {type: "text/plain"});
             saveAs(blob, "iteminfo.lua");
         }, false);
-    }   
-    
+    }
+
 }
